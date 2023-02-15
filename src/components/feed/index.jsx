b60fad45c44acf658cfeaad5b4bed7421190a2fa
natashasilveira <natashasilveira.p@gmail.com>
2023-02-15 00:25:02 -0300

    Simulando erro

--- src/components/feed/index.jsx
+++ src/components/feed/index.jsx
@@ -6,15 +6,20 @@ import { Text } from '../../ui/text'
 export const Feed = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     const makeRequest = async () => {
-      setIsLoading(true);
-      const response = await fetcher('photos');
-      setTimeout(() => {
-        setIsLoading(false);
-        setItems(response);
-      }, 2000);
+      try {
+        setIsLoading(true);
+        const response = await fetcher('photos');
+        setTimeout(() => {
+          setIsLoading(false);
+          setItems(response);
+        }, 2000);
+      } catch (error) {
+        console.log(`Deu Ruim: ${error}`);
+      }
     }
 
     makeRequest();
@@ -33,9 +38,9 @@ export const Feed = () => {
   return (
     <S.Wrapper>
       {isLoading && <Text>Carregando...</Text>}
-      {items.map(item => 
+      {items.map(item =>
         <S.Item key={item.id}>
-          <S.Image src={item.urls.small} /> 
+          <S.Image src={item.urls.small} />
         </S.Item>
       )}
     </S.Wrapper>
--- src/components/services/config.js
+++ src/components/services/config.js
@@ -1,4 +1,4 @@
 
 export const accessKey = 'yJbQ21EEzW-PT-zj2rhdAqfR73QRyZG9uKg4DihSFJQ'
-export const BASE_URL = 'https://api.unsplash.com'
+export const BASE_URL = 'https://apii.unsplash.com'
 // const BASE_URL = `https://api.unsplash.com/photos?client_id=${acessKey}`
